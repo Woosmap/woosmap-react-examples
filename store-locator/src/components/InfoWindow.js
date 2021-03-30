a24373cb95fe19aeecd71297a52e22f1db381b12
gaelsimon <gsimon@webgeoservices.com>
2021-03-30 14:56:23 +0200

    Added attributes in infoWindow

--- store-locator/src/components/InfoWindow.js
+++ store-locator/src/components/InfoWindow.js
@@ -1,15 +1,28 @@
 import "./InfoWindow.css";
 
-export default function InfoWindow({
-                                       name,
-                                       addressCity,
-                                       contactPhone
-                                   }) {
+export default function InfoWindow({name, address, contact, openingHours}) {
+    let openingLabel;
+    if (openingHours && openingHours.open_now) {
+        openingLabel = openingHours.current_slice["all-day"] ? "24/24" : openingHours.current_slice.start + "–" + openingHours.current_slice.end;
+    }
     return (
-        <div className="store-infowindow">
-            <div>{name}</div>
-            <div>{addressCity}</div>
-            <div>{contactPhone}</div>
+        <div className="infoWindow">
+            <div className="infoWindow__storeName">{name}</div>
+            {address && <div>
+                <h4 className="infoWindow__addressTitle">Address</h4>
+                <div className="infoWindow__addressLines">{address.lines}</div>
+                <div className="infoWindow__addressCity">{address.city}</div>
+            </div>}
+            {contact && <div>
+                <h4 className="infoWindow__contactTitle">Contact</h4>
+                <div className="infoWindow__contactPhone">{contact.phone}</div>
+                <div className="infoWindow__contactWebsite"><a href={contact.website} target='_blank'>go to website</a>
+                </div>
+            </div>}
+            {openingLabel &&
+            <div className="infoWindow__openingHours">
+                Open now: {openingLabel}
+            </div>}
         </div>
     );
 }
