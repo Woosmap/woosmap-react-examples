8363e9bbd6635040c74ce8c00c3da673f150e863
gaelsimon <gsimon@webgeoservices.com>
2021-04-19 16:59:55 +0200

    Check if Script is already loaded before loading it.

--- store-locator/src/hooks/useScript.js
+++ store-locator/src/hooks/useScript.js
@@ -4,13 +4,18 @@ export default function useScript(src) {
     try {
         const [loaded, setLoaded] = useState(false);
         useEffect(() => {
-            const script = document.createElement('script');
-            script.src = src;
-            script.async = true;
-            script.onload = function () {
+            let script = document.querySelector(`script[src="${src}"]`);
+            if (!script) {
+                script = document.createElement('script');
+                script.src = src;
+                script.async = true;
+                script.onload = function () {
+                    setLoaded(true)
+                }
+                document.body.appendChild(script);
+            } else {
                 setLoaded(true)
             }
-            document.body.appendChild(script);
             return () => {
                 document.body.removeChild(script);
             }
